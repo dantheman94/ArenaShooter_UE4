3912ea87f4a3c9cd9b7227506c00656e66063042
DanMarton <dmarton94@gmail.com>
2020-03-02 18:03:56 +1100

    Added team properties struct to gamemode class

--- ArenaShooter 4.23/Source/ArenaShooter/Public/Game/BaseGameMode.h
+++ ArenaShooter 4.23/Source/ArenaShooter/Public/Game/BaseGameMode.h
@@ -6,12 +6,49 @@
 #include "GameFramework/GameModeBase.h"
 #include "BaseGameMode.generated.h"
 
-/**
- * 
- */
+// *** ENUMS
+
+UENUM(BlueprintType)
+enum class E_TeamNames : uint8
+{
+	eTN_Alpha UMETA(DisplayName = "Alpha"),
+	eTN_Bravo UMETA(DisplayName = "Bravo"),
+	eTN_Charlie UMETA(DisplayName = "Charlie"),
+	eTN_Delta UMETA(DisplayName = "Delta")
+};
+
+// *** STRUCTS
+
+USTRUCT(BlueprintType)
+struct FTeamProperties
+{
+	GENERATED_BODY()
+
+		FTeamProperties() {}
+
+public:
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		E_TeamNames _eTeam = E_TeamNames::eTN_Alpha;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		FLinearColor _cTeamColour = FLinearColor::White;
+
+	E_TeamNames GetTeam()
+	{ return _eTeam; }
+
+	FLinearColor GetTeamColour()
+	{ return _cTeamColour; }
+
+};
+
+// *** CLASSES
+
 UCLASS()
 class ARENASHOOTER_API ABaseGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+public:
+
 };
