00592439572a0ccc47a636d3927dcb162a41af24
DANS-PC\Daniel <dmarton94@gmail.com>
2020-03-10 01:12:50 +1100

    Added player popup widget as well as a matchmaking home widget (unused)

---
+++ ArenaShooter 4.23/Source/ArenaShooter/Public/Game/UserInterface/ButtonMainMenu.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ButtonMainMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENASHOOTER_API UButtonMainMenu : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
